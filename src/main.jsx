4431b398a24c3a1356678c64f1c234b8e95892d6
Hakim-2206 <hakim.hakim2206@gmail.com>
2025-02-10 18:39:25 +0100

    First commit

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import {StrictMode} from 'react'
+import {BrowserRouter} from "react-router-dom";
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import PlayerContextProvider from "./context/PlayerContext.jsx";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <BrowserRouter>
+            <PlayerContextProvider>
+                <App/>
+            </PlayerContextProvider>
+        </BrowserRouter>
+    </StrictMode>,
+)
